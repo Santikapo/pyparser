f16af75d96d9cd1f4d055122a2c4e644c96a3fe5
Santikapo <santiagoj.inbox@gmail.com>
2024-01-20 22:39:19 -0800

    reading ethertypes

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ import binascii
 https = 443
 
 interface = "wlp2s0"
+eth_length = 14
+
+
+
+# EtherTypes
+ethtypes = {
+        2048    : "IPv4",
+        2054    : "ARP"
+}
 
 def main():
     # @todo 
@@ -27,15 +36,30 @@ def main():
             # recieve packet
             packet = mysocket.recvfrom(65535)
             packet = packet[0]
-            eth_length = 14
-
+            
+            # unpacking ethernet header
             eth_header = packet[:eth_length]
             eth_header = struct.unpack("!6c6c2c", eth_header)
+
+            # destination mac-address
             dest = []
             for a in eth_header[0:6]:
                 a = int.from_bytes(a, "big")
                 dest.append(hex(a).lstrip('0x'))
-            print(':'.join(dest))
+            dest = ':'.join(dest)
+
+            # source mac-address
+            source = []
+            for a in eth_header[6:12]:
+                a = int.from_bytes(a, "big")
+                source.append(hex(a).lstrip('0x'))
+            source = ':'.join(source)
+
+            ethertype = int.from_bytes(packet[12:14], "big")
+            ethertype = 40
+            print(f'Destination MAC: {dest}')
+            print(f'Source MAC: {source}')
+            print(f'EtherType: {ethtypes[ethertype]}')
             print()
     except KeyboardInterrupt:
         print("\nParser stopped")
